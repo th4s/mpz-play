849fe1a64d3a693c70eb419b9f954f4932645615
th4s <th4s@metavoid.xyz>
2024-07-04 16:11:48 +0200

    Created shell for garbled-circuits.

---
+++ 02-garbled-circuits/src/bob.rs
@@ -0,0 +1,12 @@
+use common::{tcp_mux, Role, DEFAULT_LOCAL};
+
+#[tokio::main]
+async fn main() {
+    // Open connection and poll it in the background.
+    let (future, mut ctrl) = tcp_mux(Role::Bob, DEFAULT_LOCAL).await.unwrap();
+    let join_handle = tokio::spawn(future);
+
+    // Properly close the connection.
+    ctrl.mux_mut().close();
+    join_handle.await.unwrap().unwrap();
+}
