680b93e7e0e0cf38d362353fdbfff63e8678b339
th4s <th4s@metavoid.xyz>
2024-07-04 13:39:29 +0200

    Added `01-oblivious-transfer` code for Alice and Bob.

--- 01-oblivious-transfer/src/alice.rs
+++ 01-oblivious-transfer/src/alice.rs
@@ -1,10 +1,32 @@
 use common::{tcp_mux, Role, DEFAULT_LOCAL};
+use mpz_common::executor::MTExecutor;
+use mpz_core::Block;
+use mpz_ot::{
+    chou_orlandi::{Sender, SenderConfig},
+    OTSender, OTSetup,
+};
 
 #[tokio::main]
 async fn main() {
     // Open connection and poll it in the background.
     let (future, mut ctrl) = tcp_mux(Role::Alice, DEFAULT_LOCAL).await.unwrap();
     let join_handle = tokio::spawn(future);
+
+    // Create an executor and spawn a context.
+    let mut executor = MTExecutor::new(ctrl.clone(), 32);
+    let mut context = executor.new_thread().await.unwrap();
+
+    // Create an OT sender and set it up.
+    let sender_config = SenderConfig::builder().build().unwrap();
+    let mut sender = Sender::new(sender_config);
+    sender.setup(&mut context).await.unwrap();
+
+    // Create a message.
+    let zero = Block::ZERO;
+    let one = Block::ONE;
+
+    // Send OTs to Bob.
+    sender.send(&mut context, &[[zero, one]]).await.unwrap();
 
     // Properly close the connection.
     ctrl.mux_mut().close();
--- 01-oblivious-transfer/src/bob.rs
+++ 01-oblivious-transfer/src/bob.rs
@@ -1,10 +1,31 @@
 use common::{tcp_mux, Role, DEFAULT_LOCAL};
+use mpz_common::executor::MTExecutor;
+use mpz_ot::{
+    chou_orlandi::{Receiver, ReceiverConfig},
+    OTReceiver, OTSetup,
+};
 
 #[tokio::main]
 async fn main() {
     // Open connection and poll it in the background.
     let (future, mut ctrl) = tcp_mux(Role::Bob, DEFAULT_LOCAL).await.unwrap();
     let join_handle = tokio::spawn(future);
+
+    // Create an executor and spawn a context.
+    let mut executor = MTExecutor::new(ctrl.clone(), 32);
+    let mut context = executor.new_thread().await.unwrap();
+
+    // Create an OT receiver and set it up.
+    let receiver_config = ReceiverConfig::builder().build().unwrap();
+    let mut receiver = Receiver::new(receiver_config);
+    receiver.setup(&mut context).await.unwrap();
+
+    // Create a choice.
+    let choice = true;
+
+    // Receive OTs from Alice.
+    let output = receiver.receive(&mut context, &[choice]).await.unwrap();
+    println!("Received from Alice: {:?}", output.msgs.first().unwrap());
 
     // Properly close the connection.
     ctrl.mux_mut().close();
