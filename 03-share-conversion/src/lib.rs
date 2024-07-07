9dd99fe1736e059a0c6e32f6644f8d3d7e9786fc
th4s <th4s@metavoid.xyz>
2024-07-07 14:40:35 +0200

    Add description for `03-share-conversion`.

--- 03-share-conversion/src/lib.rs
+++ 03-share-conversion/src/lib.rs
@@ -1,3 +1,15 @@
+//! Now we want to explore what mpz has to offer for finite fields. We want to use
+//! [`mpz_share_conversion`] in order to convert a shared sum into a shared product and again back
+//! into a shared sum.
+//!
+//! To achieve that, depending on your role instantiate an [`mpz_ole::rot::OLESender`] or an
+//! [`mpz_ole::rot::OLEReceiver`]. They can be used for creating an
+//! [`mpz_share_conversion::ShareConversionSender`]/[mpz_share_conversion::ShareConversionReceiver].
+//! Then you can use [`mpz_share_conversion::AdditiveToMultiplicative`] to convert the shared sum
+//! into a shared product and [`mpz_share_conversion::MultiplicativeToAdditive`] to convert it back
+//! again. In the end check that both sums are equal by sending over the missing summands to each
+//! other.
+
 use anyhow::Error as Anyhow;
 use common::MuxControl;
 use mpz_common::executor::MTExecutor;
