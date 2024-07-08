51dd2122f9eb5dd4a778cbd11560200a557fa808
th4s <th4s@metavoid.xyz>
2024-07-08 15:46:24 +0200

    Rename `share-conversion` -> `finite-fields`.

--- 03-finite-fields/src/alice.rs
+++ 03-finite-fields/src/alice.rs
@@ -1,41 +1,41 @@
 use common::{tcp_mux, FramedUidMux, Role, DEFAULT_LOCAL};
+use finite_fields::setup_ot_sender;
 use mpz_common::{executor::MTExecutor, Allocate, Preprocess};
 use mpz_fields::p256::P256;
-use mpz_ole::rot::OLEReceiver;
+use mpz_ole::rot::OLESender;
 use mpz_share_conversion::{
-    AdditiveToMultiplicative, MultiplicativeToAdditive, ShareConversionReceiver,
+    AdditiveToMultiplicative, MultiplicativeToAdditive, ShareConversionSender,
 };
 use serio::{stream::IoStreamExt, SinkExt};
-use share_conversion::setup_ot_receiver;
 
 #[tokio::main]
 async fn main() {
     // Open connection and poll it in the background.
-    let (future, mut ctrl) = tcp_mux(Role::Bob, DEFAULT_LOCAL).await.unwrap();
+    let (future, mut ctrl) = tcp_mux(Role::Alice, DEFAULT_LOCAL).await.unwrap();
     let join_handle = tokio::spawn(future);
 
     // Create an executor and setup OT.
     let mut executor = MTExecutor::new(ctrl.clone(), 32);
-    let ot_receiver = setup_ot_receiver(&mut executor).await.unwrap();
+    let ot_sender = setup_ot_sender(&mut executor).await.unwrap();
 
     // Setup OLE and share conversion.
     let mut context = executor.new_thread().await.unwrap();
-    let mut ole_receiver = OLEReceiver::<_, P256>::new(ot_receiver);
-    ole_receiver.alloc(2);
-    ole_receiver.preprocess(&mut context).await.unwrap();
+    let mut ole_sender = OLESender::<_, P256>::new(ot_sender);
+    ole_sender.alloc(2);
+    ole_sender.preprocess(&mut context).await.unwrap();
 
-    let mut receiver = ShareConversionReceiver::<_, P256>::new(ole_receiver);
+    let mut sender = ShareConversionSender::<_, P256>::new(ole_sender);
 
     // Choose a number.
-    let number = P256::new(3).unwrap();
+    let number = P256::new(42).unwrap();
 
     // Perform the conversion.
-    let factor = receiver
+    let factor = sender
         .to_multiplicative(&mut context, vec![number])
         .await
         .unwrap();
 
-    let summand = receiver
+    let summand = sender
         .to_additive(&mut context, factor)
         .await
         .unwrap()
