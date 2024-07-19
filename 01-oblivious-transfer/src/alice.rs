33a8f5bdf8f9db1dc8c66375a27d4deecab94c10
th4s <th4s@metavoid.xyz>
2024-07-19 11:58:02 +0200

    React oblivious transfer.

--- 01-oblivious-transfer/src/alice.rs
+++ 01-oblivious-transfer/src/alice.rs
@@ -1,31 +1,12 @@
-use common::{tcp_connect, Role, DEFAULT_LOCAL};
-use mpz_common::executor::STExecutor;
-use mpz_core::Block;
-use mpz_ot::{
-    chou_orlandi::{Sender, SenderConfig},
-    OTSender, OTSetup,
-};
-use serio::codec::{Bincode, Codec};
-
 #[tokio::main]
 async fn main() {
-    // Open a connection.
-    let tcp = tcp_connect(Role::Alice, DEFAULT_LOCAL).await.unwrap();
-    let channel = Bincode::default().new_framed(tcp);
+    // Open a connection and create a channel.
 
     // Create an executor.
-    let mut executor = STExecutor::new(channel);
 
     // Create an OT sender and set it up.
-    let sender_config = SenderConfig::default();
-    let mut sender = Sender::new(sender_config);
-
-    sender.setup(&mut executor).await.unwrap();
 
     // Create messages.
-    let zero = Block::ZERO;
-    let one = Block::ONE;
 
     // Send OTs to Bob.
-    sender.send(&mut executor, &[[zero, one]]).await.unwrap();
 }
--- 01-oblivious-transfer/src/bob.rs
+++ 01-oblivious-transfer/src/bob.rs
@@ -1,30 +1,12 @@
-use common::{tcp_connect, Role, DEFAULT_LOCAL};
-use mpz_common::executor::STExecutor;
-use mpz_ot::{
-    chou_orlandi::{Receiver, ReceiverConfig},
-    OTReceiver, OTSetup,
-};
-use serio::codec::{Bincode, Codec};
-
 #[tokio::main]
 async fn main() {
-    // Open a connection.
-    let tcp = tcp_connect(Role::Bob, DEFAULT_LOCAL).await.unwrap();
-    let channel = Bincode::default().new_framed(tcp);
+    // Open a connection and create a channel.
 
     // Create an executor.
-    let mut executor = STExecutor::new(channel);
 
     // Create an OT receiver and set it up.
-    let receiver_config = ReceiverConfig::default();
-    let mut receiver = Receiver::new(receiver_config);
-
-    receiver.setup(&mut executor).await.unwrap();
 
     // Make a choice.
-    let choice = true;
 
     // Receive OTs from Alice.
-    let output = receiver.receive(&mut executor, &[choice]).await.unwrap();
-    println!("Received from Alice: {:?}", output.msgs.first().unwrap());
 }
