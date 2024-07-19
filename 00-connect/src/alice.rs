8360fea4a58200cf48b12a0b96466fb2d59da0c7
th4s <th4s@metavoid.xyz>
2024-07-19 11:52:42 +0200

    Redact `connect` exercise.

--- 00-connect/src/alice.rs
+++ 00-connect/src/alice.rs
@@ -1,19 +1,8 @@
-use common::{tcp_connect, Role, DEFAULT_LOCAL};
-use serio::{
-    codec::{Bincode, Codec},
-    stream::IoStreamExt,
-    SinkExt,
-};
-
 #[tokio::main]
 async fn main() {
     // Open a connection.
-    let tcp = tcp_connect(Role::Alice, DEFAULT_LOCAL).await.unwrap();
-    let mut channel = Bincode::default().new_framed(tcp);
 
     // Send a number to Bob and wait for Bob's number.
-    channel.send(42_u32).await.unwrap();
-    let received: u32 = channel.expect_next().await.unwrap();
 
-    println!("Alice received: {received}");
+    // Print the number Alice received.
 }
--- 00-connect/src/bob.rs
+++ 00-connect/src/bob.rs
@@ -1,20 +1,8 @@
-use common::{tcp_connect, Role, DEFAULT_LOCAL};
-use serio::{
-    codec::{Bincode, Codec},
-    stream::IoStreamExt,
-    SinkExt,
-};
-
 #[tokio::main]
 async fn main() {
     // Open a connection.
-    let tcp = tcp_connect(Role::Bob, DEFAULT_LOCAL).await.unwrap();
-    let mut channel = Bincode::default().new_framed(tcp);
 
-    // Wait for Alice to send her number, then increment and send it back.
-    let mut received: u32 = channel.expect_next().await.unwrap();
-    println!("Bob received: {received}");
+    // Wait for Alice to send her number and print it out.
 
-    received += 1;
-    channel.send(received).await.unwrap();
+    // Increment and send it back.
 }
